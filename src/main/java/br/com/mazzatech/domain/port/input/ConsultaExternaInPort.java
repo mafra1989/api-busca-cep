143b9cea08ef6d692558f1d0268debeef11849fb
Evandro <evandro.mafra1989@gmail.com>
2023-07-22 10:09:35 -0400

    Inclusão da assinatura de exceptions no método consultaCep da interface ConsultaExternaInPort

--- src/main/java/br/com/mazzatech/domain/port/input/ConsultaExternaInPort.java
+++ src/main/java/br/com/mazzatech/domain/port/input/ConsultaExternaInPort.java
@@ -2,7 +2,9 @@ package br.com.mazzatech.domain.port.input;
 
 import br.com.mazzatech.domain.model.CepDomain;
 
+import java.util.concurrent.ExecutionException;
+
 public interface ConsultaExternaInPort {
 
-    CepDomain consultaCep(Long code);
+    CepDomain consultaCep(Long code) throws ExecutionException, InterruptedException;
 }
